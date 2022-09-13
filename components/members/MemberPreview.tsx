10932c97c8617416a15e31902664120400bd5fb8
jettblu <jettblu.fabrications@gmail.com>
2022-09-12 23:57:17 -0400

    Update member photo hover style

--- components/members/MemberPreview.tsx
+++ components/members/MemberPreview.tsx
@@ -14,7 +14,7 @@ const MemberPreview:NextPage<Props> = (props) => {
     return(
         <div className="">
             <div className="mx-auto hover:z-10 transition ease-in-out hover:scale-110" onClick={()=>handleMemberClicked()}>
-                <img src={member.photoPath} className="hover:saturate-0 w-64 h-64 rounded-md drop-shadow-lg object-cover"/>
+                <img src={member.photoPath} className="hover:shadow-md hover:shadow-purple-400 w-64 h-64 rounded-md drop-shadow-lg object-cover"/>
                 <div className="flex">
                     <p className="my-2 text-lg text-gray-300 dark:text-gray-400 font-semibold">{member.name}</p>
                 </div>
