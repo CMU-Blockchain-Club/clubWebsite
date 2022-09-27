6d46795a80339f3290d152cc7b86a0f705a43a70
jettblu <jettblu.fabrications@gmail.com>
2022-09-27 16:57:23 -0400

    Update main members page

--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -6,10 +6,14 @@ import ProjectPreview from '../../components/projects/ProjectPreview'
 import { CurrentProjects } from '../../src/labs/projects'
 import { IProject } from '../../src/labs/projects/models'
 import Link from 'next/link'
+import { IMember } from '../../src/members/models'
+import { Members22And23 } from '../../src/members'
+import MiniMemberPreview from '../../components/members/MiniMemberPreview'
 
 
 
 const Members: NextPage = () => {
+  const membersToDisplay:IMember[] = Members22And23;
   return (
     <div className="">
       {/* <Head>
@@ -28,10 +32,43 @@ const Members: NextPage = () => {
       <h1 className="text-5xl font-bold mb-2">
         Members
       </h1>
-      <p className="text-xl text-slate-400 dark:text-slate-300">The chosen few who make it happen.</p>
+      <p className="text-lg text-slate-400 dark:text-slate-300">The Researchers and Builders Who Make it Happen </p>
     </div>
-    <div className="text-xl text-black dark:text-white mb-8 leading-loose">
-        <p className="pt-2 pb-2">If you're interested in researching blockchains or building new applications, join the <span className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">CMU Blockchain Group</span>. Our mission is to build and understand the decentralized future. As a member you'll have access to:</p>
+
+    
+      <div className="my-4">
+      <h1 className="text-3xl font-bold text-transparent font-bold bg-clip-text bg-gradient-to-b from-purple-400 to-pink-600">2022-23 Class</h1>
+      <p className="text-lg text-black dark:text-white">A few of the researchers and builders from our latest class of applicants.</p>
+      <p className="text-lg text-black dark:text-white"> Hop to the <span className="text-purple-500 font-semibold"><Link href="../members/portal">member portal</Link></span> for current opportunities.</p>
+      </div>
+      
+      <div className="grid my-6 grid-cols-1 md:grid-cols-2 gap-5 md:gap-3 mx-auto place-items-center">
+        {
+          membersToDisplay.length != 0 &&
+          (membersToDisplay).map((member:IMember, index:number)=>
+                    <MiniMemberPreview member={member} key={index}/>
+          )
+        }
+      </div>
+        
+      
+        {/* project add form */}
+        <div className="mx-auto">
+          <PopupButton
+            id="L4JePZlc"
+            size={80}
+            onReady={() => {
+              console.log('Member profile form ready')
+            }}
+            className="font-bold bg-gradient-to-r from-purple-400 to-pink-600 p-2 rounded-lg text-slate-600 dark:text-slate-200 transition ease-in-out hover:scale-105 text-xl mx-auto"
+          >
+          Add Your Profile
+          </PopupButton>
+        </div>
+
+        <div className="text-xl text-black dark:text-white mt-8 mb-8 leading-loose">
+        <h2 className='text-2xl font-bold text-gray-800 dark:text-gray-200'>Member Perks</h2>
+        <p className="pt-2 pb-2">Our mission is to build and understand the decentralized future. As a member of the <span className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">CMU Blockchain Group</span> you have access to:</p>
       <ol className="list-decimal pl-8">
         <li className="text-gray-800 dark:text-gray-400 font-semibold hover:text-purple-500">Club sponsored conferences</li>
         <li className="text-gray-800 dark:text-gray-400 font-semibold hover:text-green-500">Member only dinners</li>
@@ -40,23 +77,6 @@ const Members: NextPage = () => {
         <li className="text-gray-800 dark:text-gray-400 font-semibold hover:text-orange-500">Industry job opportunities</li>
       </ol>
     </div>
-    If you are already a member, go <span className="text-purple-500 font-semibold"><Link href="../members/portal">here</Link></span>.
-
-        {/* project add form */}
-        <div className="mx-auto">
-          <PopupButton
-            id="e6FFEeGR"
-            size={80}
-            onReady={() => {
-              console.log('Member application form ready')
-            }}
-            className="font-bold bg-gradient-to-r from-purple-400 to-pink-600 p-2 rounded-lg text-slate-600 dark:text-slate-200 transition ease-in-out hover:scale-105 text-xl mx-auto"
-          >
-          Apply Now
-          </PopupButton>
-        </div>
-
-      <p className="text-slate-300 dark:text-slate-400 my-2 text-md">Applications for the fall semester are now <span className="font-bold">closed</span>.</p>
       
 
      </main>
