5dffb881857d15cb346260bff751eb442db87c38
jettblu <jettblu.fabrications@gmail.com>
2022-10-01 12:28:53 -0400

    Add WalletConnect bounty

--- src/bounties/index.ts
+++ src/bounties/index.ts
@@ -8,9 +8,21 @@ export const CurrentBounties:IBounty[] = [
         },
         task:{
             name: "Add Cosmos Address Generator",
-            description:"Update a heirarchical deterministic seedloop, to generate standard Cosmos addresses."
+            description:"Update a key manager to generate standard Cosmos addresses."
         },
         link:"https://github.com/KryptikApp/kryptik-seedloop/issues/1",
         prize:"$200"
+    },
+    {
+        sponsor:{
+            name: "CMU Blockchain Group",
+            photoPath: "/cbgLogo.png"
+        },
+        task:{
+            name: "Add WalletConnect",
+            description:"Allow users to connect their mobile wallet to a sample dApp."
+        },
+        link:"https://github.com/CMU-Blockchain-Club/dappStarter/issues/1",
+        prize:"$50"
     }
 ]
