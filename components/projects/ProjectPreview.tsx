e702f890b93f53e7379f691ceec492be8ffffc85
jettblu <jettblu.fabrications@gmail.com>
2022-10-01 13:05:42 -0400

    Update project preview style

--- components/projects/ProjectPreview.tsx
+++ components/projects/ProjectPreview.tsx
@@ -18,9 +18,8 @@ const ProjectPreview:NextPage<Props> = (props) => {
 
     return(
         <div className="">
-            <div className="mx-auto hover:z-10 rounded-md drop-shadow-lg border border-gray-200 bg-slate-50 dark:bg-black transition ease-in-out hover:scale-105 max-w-[400px] py-4 min-h-[335px]" onClick={()=>handleMemberClicked()}>
-                <div className="">
-                    <div className="flex flex-col space-y-4 px-2">
+            <div className="mx-auto hover:z-10 rounded-md drop-shadow-lg border border-gray-200 bg-slate-50 dark:bg-black transition ease-in-out hover:scale-105 max-w-[400px] py-4 h-[335px]" onClick={()=>handleMemberClicked()}>
+                    <div className="flex flex-col space-y-4 px-2 h-full">
                         <div className="flex flex-row">
                             <div className="my-2">
                                 <img src={project.photoPath} className="inline rounded-full w-10 h-10 object-cover dark:bg-black"/>
@@ -28,8 +27,8 @@ const ProjectPreview:NextPage<Props> = (props) => {
                             </div>
                         </div>
                         <p className="text-lg text-slate-800 dark:text-slate-200">{project.description}</p>
-                        <div className="mt-2">
-                                <div className="float-right">
+                        <div className="flex-grow relative">
+                                <div className="float-right absolute bottom-0 right-0">
                                     {
                                         project.socials && 
                                         <Socials socials={project.socials}/>
@@ -38,7 +37,6 @@ const ProjectPreview:NextPage<Props> = (props) => {
                         </div>
                     </div>
 
-                </div>
             </div>
         </div>
     )
