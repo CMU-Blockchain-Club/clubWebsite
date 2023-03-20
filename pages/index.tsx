2c26c0cb3ec26bcda6aa3ebb518d68a7f88896ce
jettblu <jettblu.fabrications@gmail.com>
2023-03-20 16:11:54 -0400

    Update leaders display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,129 +1,129 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import DoCard from '../components/DoCard'
-import Footer from '../components/Footer'
-import MemberPreview from '../components/members/MemberPreview'
-import OfficerPreview from '../components/members/OfficerPreview'
+import type { NextPage } from "next";
+import Head from "next/head";
+import DoCard from "../components/DoCard";
+import Footer from "../components/Footer";
+import MemberPreview from "../components/members/MemberPreview";
+import OfficerPreview from "../components/members/OfficerPreview";
 
-import PartnerPreview from '../components/partners/PartnerPreview'
-import { FacultyAdvisors } from '../src/advisors'
-import { IAdvisor } from '../src/advisors/models'
-import { CMUBrand } from '../src/brand'
-import { Board23Spring } from '../src/members'
-import { IBoardMember } from '../src/members/models'
-import { ActivePartners } from '../src/partners'
-import { IPartner } from '../src/partners/models'
-
+import PartnerPreview from "../components/partners/PartnerPreview";
+import { FacultyAdvisors } from "../src/advisors";
+import { IAdvisor } from "../src/advisors/models";
+import { CMUBrand } from "../src/brand";
+import { Board23Spring } from "../src/members";
+import { IBoardMember } from "../src/members/models";
+import { ActivePartners } from "../src/partners";
+import { IPartner } from "../src/partners/models";
 
 const Home: NextPage = () => {
   return (
     <div className="">
-
-     <main className="">
-
+      <main className="">
         <div className="min-h-[100vh] mx-auto dark:bg-black dark:text-white">
-          <div className="h-[15vh] md:h-[16vh] lg:h-[20vh] xl:h-[25vh]">
-
-          </div>
+          <div className="h-[15vh] md:h-[16vh] lg:h-[20vh] xl:h-[25vh]"></div>
           <div className="max-w-5xl mx-auto px-4">
-
-          <div className="flex flex-col md:flex-row space-x-10 space-y-12">
-            <div className="">
-              <img src="/cbgLogo.png" className="max-w-[90%] md:max-w-[500px] h-auto mx-auto"/>
-            </div>
-            <div className="flex-grow mx-auto">
-              <div className="md:h-[50px]">
-
+            <div className="flex flex-col md:flex-row space-x-10 space-y-12">
+              <div className="">
+                <img
+                  src="/cbgLogo.png"
+                  className="max-w-[90%] md:max-w-[500px] h-auto mx-auto"
+                />
               </div>
-              <div className="mx-auto">
-                <h1 className="text-4xl xl:text-5xl font-bold mx-auto">CMU Blockchain Group</h1>
-                <p className="text-lg font-semibold text-gray-600 dark:text-gray-400">Creating the future one block at a time.</p>
+              <div className="flex-grow mx-auto">
+                <div className="md:h-[50px]"></div>
+                <div className="mx-auto">
+                  <h1 className="text-4xl xl:text-5xl font-bold mx-auto">
+                    CMU Blockchain Group
+                  </h1>
+                  <p className="text-lg font-semibold text-gray-600 dark:text-gray-400">
+                    Creating the future one block at a time.
+                  </p>
+                </div>
               </div>
-              
             </div>
           </div>
-
-          </div>
-          
         </div>
         <div className="bg-black max-w-screen text-white dark:bg-white dark:text-black">
-
           <div className="max-w-5xl mx-auto pt-8 pb-20">
             <div className="flex-col md:flex-row">
               <div className="text-center font-bold text-6xl font-bold mb-8">
                 Our Partners
               </div>
               <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto place-items-center">
-              {
-                ActivePartners.length != 0 &&
-                (ActivePartners).map((partner:IPartner, index:number)=>
-                          <PartnerPreview partner={partner} key={index}/>
-                )
-              }
+                {ActivePartners.length != 0 &&
+                  ActivePartners.map((partner: IPartner, index: number) => (
+                    <PartnerPreview partner={partner} key={index} />
+                  ))}
               </div>
             </div>
           </div>
-      
         </div>
 
         <div className="max-w-screen dark:bg-black dark:text-white">
-
           <div className="max-w-5xl mx-auto pt-8 pb-20">
             <div className="flex-col md:flex-row">
               <div className="text-center font-bold text-6xl font-bold mb-8">
                 What We Do
               </div>
               <div className="grid grid-cols-1 md:grid-cols-3 mx-auto gap-4 md:gap-8 px-8 md:px-0">
-               
+                <DoCard
+                  title={"101"}
+                  description={
+                    "Senior members deliver weekly education sessions that simplify the complex world of blockchain."
+                  }
+                  hexColor={CMUBrand.green}
+                />
 
-                <DoCard title={"101"} description={"Senior members deliver weekly education sessions that simplify the complex world of blockchain."} hexColor={CMUBrand.green}/>
+                <DoCard
+                  title={"Labs"}
+                  description={
+                    "An incubator for CMU students to transform ideas into code that changes the world. Project teams are supported by community grants and in house advisors."
+                  }
+                  hexColor={CMUBrand.red}
+                />
 
-                <DoCard title={"Labs"} description={"An incubator for CMU students to transform ideas into code that changes the world. Project teams are supported by community grants and in house advisors."} hexColor={CMUBrand.red}/>
-
-                <DoCard title={"Network"} description={"We connect the best blockchain companies with the brightest minds from Carnegie Mellon."} hexColor={CMUBrand.blue}/>
-         
-          
-
+                <DoCard
+                  title={"Network"}
+                  description={
+                    "We connect the best blockchain companies with the brightest minds from Carnegie Mellon."
+                  }
+                  hexColor={CMUBrand.blue}
+                />
               </div>
             </div>
           </div>
-      
         </div>
         <div className="bg-black max-w-screen text-white pt-8 pb-20 dark:bg-white dark:text-black">
-        <div className='max-w-4xl mx-auto pb-8'>
+          <div className="max-w-4xl mx-auto pb-8">
             <div className="flex-col md:flex-row">
               <div className="text-center font-bold text-6xl font-bold mb-8">
-                    Team
+                Team
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-3 mx-auto place-items-center">
-                  {
-                    Board23Spring.length != 0 &&
-                    (Board23Spring).map((boardMember:IBoardMember, index:number)=>
-                              <OfficerPreview member={boardMember} key={index}/>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-3 mx-auto place-items-center">
+                {Board23Spring.length != 0 &&
+                  Board23Spring.map(
+                    (boardMember: IBoardMember, index: number) => (
+                      <OfficerPreview member={boardMember} key={index} />
                     )
-                  }
+                  )}
               </div>
               <div className="text-center font-bold text-5xl font-bold my-12">
-                    Advisors
+                Advisors
               </div>
               <div className="grid grid-cols-1  mx-auto place-items-center">
-                  {
-                    FacultyAdvisors.length != 0 &&
-                    (FacultyAdvisors).map((facultyAdvisor:IAdvisor, index:number)=>
-                              <MemberPreview member={facultyAdvisor} key={index}/>
+                {FacultyAdvisors.length != 0 &&
+                  FacultyAdvisors.map(
+                    (facultyAdvisor: IAdvisor, index: number) => (
+                      <MemberPreview member={facultyAdvisor} key={index} />
                     )
-                  }
+                  )}
               </div>
+            </div>
+          </div>
         </div>
-        </div>
-        
-      </div>
-      <Footer/>
-
-     </main>
-
+        <Footer />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/members/index.ts
+++ src/members/index.ts
@@ -1,265 +1,264 @@
 import { BoardPosition, IBoardMember, IMember } from "./models";
 
-export const Board2022:IBoardMember[] = [
-    {
-        name: "Jett Hays",
-        photoPath: "/media/leaders/board2122/jett.jpg",
-        boardData:{
-            position: BoardPosition.vicePresident
-        },
-        socials:{}
+export const Board2022: IBoardMember[] = [
+  {
+    name: "Jett Hays",
+    photoPath: "/media/leaders/board2122/jett.jpg",
+    boardData: {
+      position: BoardPosition.vicePresident,
     },
-    {
-        name: "Nicole Sun",
-        photoPath: "/media/leaders/board2122/nicolesun.jpg",
-        boardData:{
-            position: BoardPosition.marketing
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Nicole Sun",
+    photoPath: "/media/leaders/board2122/nicolesun.jpg",
+    boardData: {
+      position: BoardPosition.marketing,
     },
-    {
-        name: "Fernanda Molina",
-        photoPath: "/media/leaders/board2122/fernanda.jpg",
-        boardData:{
-            position: BoardPosition.treasurer
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Fernanda Molina",
+    photoPath: "/media/leaders/board2122/fernanda.jpg",
+    boardData: {
+      position: BoardPosition.treasurer,
     },
-    {
-        name: "Trevor Leong",
-        photoPath: "/media/leaders/board2122/trevor.png",
-        boardData:{
-            position: BoardPosition.general
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Trevor Leong",
+    photoPath: "/media/leaders/board2122/trevor.png",
+    boardData: {
+      position: BoardPosition.general,
     },
-    {
-        name: "Xavier Xia",
-        photoPath: "/media/leaders/board2122/xavier.jpg",
-        boardData:{
-            position: BoardPosition.general
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Xavier Xia",
+    photoPath: "/media/leaders/board2122/xavier.jpg",
+    boardData: {
+      position: BoardPosition.general,
     },
-    {
-        name: "Hao Wei",
-        photoPath: "/media/leaders/board2122/hao.jpg",
-        boardData:{
-            position: BoardPosition.general
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Hao Wei",
+    photoPath: "/media/leaders/board2122/hao.jpg",
+    boardData: {
+      position: BoardPosition.general,
     },
-    {
-        name: "Ishgun Arora",
-        photoPath: "/media/leaders/board2122/ishgun.jpg",
-        boardData:{
-            position: BoardPosition.president
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Ishgun Arora",
+    photoPath: "/media/leaders/board2122/ishgun.jpg",
+    boardData: {
+      position: BoardPosition.president,
     },
-    {
-        name: "Darin Chaoui",
-        photoPath: "/media/leaders/board2122/darin.jpg",
-        boardData:{
-            position: BoardPosition.general
-        },
-        socials:{}
-    }
-]
+    socials: {},
+  },
+  {
+    name: "Darin Chaoui",
+    photoPath: "/media/leaders/board2122/darin.jpg",
+    boardData: {
+      position: BoardPosition.general,
+    },
+    socials: {},
+  },
+];
 
-export const Board22Fall:IBoardMember[] = [
-    {
-        name: "Jett Hays",
-        photoPath: "/media/leaders/board2122/jett.jpg",
-        boardData:{
-            position: BoardPosition.president
-        },
-        socials:{}
+export const Board22Fall: IBoardMember[] = [
+  {
+    name: "Jett Hays",
+    photoPath: "/media/leaders/board2122/jett.jpg",
+    boardData: {
+      position: BoardPosition.president,
     },
-    {
-        name: "Nicole Sun",
-        photoPath: "/media/leaders/board2122/nicolesun.jpg",
-        boardData:{
-            position: BoardPosition.vicePresident
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Nicole Sun",
+    photoPath: "/media/leaders/board2122/nicolesun.jpg",
+    boardData: {
+      position: BoardPosition.vicePresident,
     },
-    {
-        name: "Fernanda Molina",
-        photoPath: "/media/leaders/board2122/fernanda.jpg",
-        boardData:{
-            position: BoardPosition.treasurer
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Fernanda Molina",
+    photoPath: "/media/leaders/board2122/fernanda.jpg",
+    boardData: {
+      position: BoardPosition.treasurer,
     },
-    {
-        name: "Hao Wei",
-        photoPath: "/media/leaders/board2122/hao.jpg",
-        boardData:{
-            position: BoardPosition.governance
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Hao Wei",
+    photoPath: "/media/leaders/board2122/hao.jpg",
+    boardData: {
+      position: BoardPosition.governance,
     },
-    {
-        name: "Ishgun Arora",
-        photoPath: "/media/leaders/board2122/ishgun.jpg",
-        boardData:{
-            position: BoardPosition.general
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Ishgun Arora",
+    photoPath: "/media/leaders/board2122/ishgun.jpg",
+    boardData: {
+      position: BoardPosition.general,
     },
-    {
-        name: "Darin Chaoui",
-        photoPath: "/media/leaders/board2122/darin.jpg",
-        boardData:{
-            position: BoardPosition.marketing
-        },
-        socials:{}
-    }
-]
+    socials: {},
+  },
+  {
+    name: "Darin Chaoui",
+    photoPath: "/media/leaders/board2122/darin.jpg",
+    boardData: {
+      position: BoardPosition.marketing,
+    },
+    socials: {},
+  },
+];
 
-export const Board23Spring:IBoardMember[] = [
-    {
-        name: "Jett Hays",
-        photoPath: "/media/leaders/board2122/jett.jpg",
-        boardData:{
-            position: BoardPosition.president
-        },
-        socials:{}
-    },    
-    {
-        name: "Fernanda Molina",
-        photoPath: "/media/leaders/board2122/fernanda.jpg",
-        boardData:{
-            position: BoardPosition.treasurer
-        },
-        socials:{}
+export const Board23Spring: IBoardMember[] = [
+  {
+    name: "Jett Hays",
+    photoPath: "/media/leaders/board2122/jett2.jpg",
+    boardData: {
+      position: BoardPosition.president,
     },
-    {
-        name: "Hao Wei",
-        photoPath: "/media/leaders/board2122/hao.jpg",
-        boardData:{
-            position: BoardPosition.governance
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Fernanda Molina",
+    photoPath: "/media/leaders/board2122/fernanda.jpg",
+    boardData: {
+      position: BoardPosition.treasurer,
     },
-    {
-        name: "Ishgun Arora",
-        photoPath: "/media/leaders/board2122/ishgun.jpg",
-        boardData:{
-            position: BoardPosition.general
-        },
-        socials:{}
+    socials: {},
+  },
+  {
+    name: "Tanay Venkata",
+    photoPath: "/media/leaders/board2122/tanay.jpg",
+    boardData: {
+      position: BoardPosition.governance,
     },
-    {
-        name: "Darin Chaoui",
-        photoPath: "/media/leaders/board2122/darin.jpg",
-        boardData:{
-            position: BoardPosition.marketing
-        },
-        socials:{}
-    }
-]
+    socials: {},
+  },
+  {
+    name: "Darin Chaoui",
+    photoPath: "/media/leaders/board2122/darin.jpg",
+    boardData: {
+      position: BoardPosition.marketing,
+    },
+    socials: {},
+  },
+];
 
-export const Members22And23:IMember[] = [
-    {
-        name: "Yousif Alnajjar",
-        description: "I'm a CS freshman, mostly focused on building, but I also enjoy researching.",
-        photoPath: "/media/members/22-23/YousifAlnajjar.jpg",
-        socials: {
-            twitter:"https://twitter.com/YousifJAlnajjar",
-            github: "https://github.com/Yousif-Alnajjar"
-        }
+export const Members22And23: IMember[] = [
+  {
+    name: "Yousif Alnajjar",
+    description:
+      "I'm a CS freshman, mostly focused on building, but I also enjoy researching.",
+    photoPath: "/media/members/22-23/YousifAlnajjar.jpg",
+    socials: {
+      twitter: "https://twitter.com/YousifJAlnajjar",
+      github: "https://github.com/Yousif-Alnajjar",
     },
-    {
-        name: "Wei Wu",
-        description: "I am a video game developer & Web 3 buidler. I'm learning smart contract development.",
-        photoPath: "/media/members/22-23/WeiWu.jpeg",
-        socials: {
-            twitter: "https://twitter.com/AWW362",
-            github: "https://github.com/UndertakerW"
-        }
+  },
+  {
+    name: "Wei Wu",
+    description:
+      "I am a video game developer & Web 3 buidler. I'm learning smart contract development.",
+    photoPath: "/media/members/22-23/WeiWu.jpeg",
+    socials: {
+      twitter: "https://twitter.com/AWW362",
+      github: "https://github.com/UndertakerW",
     },
-    {
-        name: "Warren Wu",
-        description: "New but excited to web3, love skateboarding.",
-        photoPath: "/media/members/22-23/WarrenWu.jpeg",
-        socials: {
-            twitter:"https://twitter.com/Wuuuuzula",
-            github:"https://github.com/Wuuuzula"
-        }
+  },
+  {
+    name: "Warren Wu",
+    description: "New but excited to web3, love skateboarding.",
+    photoPath: "/media/members/22-23/WarrenWu.jpeg",
+    socials: {
+      twitter: "https://twitter.com/Wuuuuzula",
+      github: "https://github.com/Wuuuzula",
     },
-    {
-        name: "Benny",
-        description: "I like to code. I Love to play games ",
-        photoPath: "/media/members/22-23/Benny.png",
-        socials: {
-            github:"https://github.com/Bozebro"
-        }
+  },
+  {
+    name: "Benny",
+    description: "I like to code. I Love to play games ",
+    photoPath: "/media/members/22-23/Benny.png",
+    socials: {
+      github: "https://github.com/Bozebro",
     },
-    {
-        name: "Ram Potham",
-        description: "I'm an entrepreneurial person who loves using technology to make impactful applications. I enjoy rock climbing, running, gaming, and hackathons.",
-        photoPath: "/media/members/22-23/RamPotham.jpg",
-        socials: {
-            twitter: "https://twitter.com/PothamRam",
-            github: "https://github.com/rapturt9",
-            site: "https://rampotham.com/"
-        }
+  },
+  {
+    name: "Ram Potham",
+    description:
+      "I'm an entrepreneurial person who loves using technology to make impactful applications. I enjoy rock climbing, running, gaming, and hackathons.",
+    photoPath: "/media/members/22-23/RamPotham.jpg",
+    socials: {
+      twitter: "https://twitter.com/PothamRam",
+      github: "https://github.com/rapturt9",
+      site: "https://rampotham.com/",
     },
-    {
-        name: "Tanay Venkata",
-        photoPath: "/media/members/22-23/TanayVenkata.jpeg",
-        socials: {}
+  },
+  {
+    name: "Tanay Venkata",
+    photoPath: "/media/members/22-23/TanayVenkata.jpeg",
+    socials: {},
+  },
+  {
+    name: "Kyle Lee",
+    description:
+      "An avid explorer of both the outdoors and code. A programming fanatic enthusiastic about the future of web3.",
+    photoPath: "/media/members/22-23/KyleLee.jpeg",
+    socials: {
+      twitter: "https://twitter.com/KyleleeSea",
+      github: "https://github.com/kyleleesea",
     },
-    {
-        name: "Kyle Lee",
-        description: "An avid explorer of both the outdoors and code. A programming fanatic enthusiastic about the future of web3.",
-        photoPath: "/media/members/22-23/KyleLee.jpeg",
-        socials: {
-            twitter:"https://twitter.com/KyleleeSea",
-            github:"https://github.com/kyleleesea"
-        }
+  },
+  {
+    name: "Oliver Yao",
+    description:
+      "A striving perfectionist trying to make sense of an imperfect world. Excited to work with cutting-edge technology and build real-world solutions.",
+    photoPath: "/media/members/22-23/oliverYao.jpg",
+    socials: {
+      twitter: "https://twitter.com/KyleleeSea",
+      github: "https://github.com/kyleleesea",
     },
-    {
-        name: "Oliver Yao",
-        description: "A striving perfectionist trying to make sense of an imperfect world. Excited to work with cutting-edge technology and build real-world solutions.",
-        photoPath: "/media/members/22-23/oliverYao.jpg",
-        socials: {
-            twitter:"https://twitter.com/KyleleeSea",
-            github:"https://github.com/kyleleesea"
-        }
+  },
+  {
+    name: "Sara Patel",
+    description:
+      "Just started learning about blockchains and I'm excited to build dApps. Love poker and baking.",
+    photoPath: "/media/members/22-23/saraPatel.jpg",
+    socials: {
+      github: "https://github.com/spaddle-boat",
     },
-    {
-        name: "Sara Patel",
-        description: "Just started learning about blockchains and I'm excited to build dApps. Love poker and baking.",
-        photoPath: "/media/members/22-23/saraPatel.jpg",
-        socials: {
-            github:"https://github.com/spaddle-boat"
-        }
+  },
+  {
+    name: "Marlon Edwards",
+    description:
+      "I am a self-taught programmer that makes videos. Interested in building the future of web3.",
+    photoPath: "/media/members/22-23/marlonEdwards.png",
+    socials: {},
+  },
+  {
+    name: "Nancy Zuo",
+    description:
+      "I like to draw and write and think about human coordination problems: like how we gather, connect, and work together",
+    photoPath: "/media/members/22-23/nancyZuo.jpg",
+    socials: {
+      twitter: "https://twitter.com/nzzuo",
     },
-    {
-        name: "Marlon Edwards",
-        description: "I am a self-taught programmer that makes videos. Interested in building the future of web3.",
-        photoPath: "/media/members/22-23/marlonEdwards.png",
-        socials: {
-        }
+  },
+  {
+    name: "Chungeng Meng",
+    description:
+      "Active coder and equity trader with interests in web dev, blockchain, and fintech.",
+    photoPath: "/media/members/22-23/chungengMeng.jpg",
+    socials: {
+      github: "https://github.com/InfTkm",
     },
-    {
-        name: "Nancy Zuo",
-        description: "I like to draw and write and think about human coordination problems: like how we gather, connect, and work together",
-        photoPath: "/media/members/22-23/nancyZuo.jpg",
-        socials: {
-            twitter:"https://twitter.com/nzzuo",
-        }
-    },
-    {
-        name: "Chungeng Meng",
-        description: "Active coder and equity trader with interests in web dev, blockchain, and fintech.",
-        photoPath: "/media/members/22-23/chungengMeng.jpg",
-        socials: {
-            github:"https://github.com/InfTkm"
-        }
-    },
-
-]
+  },
+];
