96d9510a447a1ca03b54b34ce618dbc2133f03e1
jettblu <jettblu.fabrications@gmail.com>
2023-04-03 16:09:41 -0400

    Add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,29 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import type { AppProps } from "next/app";
+import { Router } from "next/router";
+import { load, trackPageview } from "fathom-client";
+import { useEffect } from "react";
+
+// Record a pageview when route changes
+Router.events.on("routeChangeComplete", (as, routeProps) => {
+  if (!routeProps.shallow) {
+    trackPageview();
+  }
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-  )
+  useEffect(() => {
+    // load fathom
+    load("PAWHTJHK", {
+      includedDomains: ["www.cmublockchain.xyz", "cmublockchain.xyz"],
+    });
+  }, []);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
